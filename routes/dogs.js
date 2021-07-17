989d49015b3a14d15b38a286fe413260fbff4bd6
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-07-16 21:40:32 -0500

    work on dogs/show.ejs

---
+++ routes/dogs.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+const router = Router()
+import * as dogsCtrl from '../controllers/dogs.js'
+
+router.get('/', dogsCtrl.index)
+
+router.get('/new', dogsCtrl.new)
+
+router.get('/:id', dogsCtrl.show)
+
+router.post('/', dogsCtrl.create)
+
+export {
+    router
+}
