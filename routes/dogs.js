d47016f760505d22c27a805b61b97201e5b95699
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-07-18 17:07:07 -0500

    add isLoggedIn middleware

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,25 +1,28 @@
 import { Router } from 'express'
-const router = Router()
 import * as dogsCtrl from '../controllers/dogs.js'
-
-router.get('/', dogsCtrl.index)
-
-router.get('/new', dogsCtrl.new)
-
-router.get('/vaccines/:dog_id', dogsCtrl.addVaccs)
-
-router.get('/behavior/:dog_id', dogsCtrl.addBehavior)
-
-router.post('/:id/vaccines', dogsCtrl.createVaccsLog)
-
-router.post('/:id/behavior', dogsCtrl.createBehaviorTag)
-
-router.get('/:id', dogsCtrl.show)
-
-router.post('/', dogsCtrl.create)
-
-router.delete('/:id', dogsCtrl.delete)
+import { isLoggedIn } from '../middleware/middleware.js'
 
 export {
     router
 }
+
+const router = Router()
+
+router.get('/', dogsCtrl.index)
+
+router.get('/new', isLoggedIn, dogsCtrl.new)
+
+router.get('/vaccines/:dog_id', isLoggedIn, dogsCtrl.addVaccs)
+
+router.get('/behavior/:dog_id', isLoggedIn, dogsCtrl.addBehavior)
+
+router.post('/:id/vaccines', isLoggedIn, dogsCtrl.createVaccsLog)
+
+router.post('/:id/behavior', isLoggedIn, dogsCtrl.createBehaviorTag)
+
+router.get('/:id', dogsCtrl.show)
+
+router.post('/', isLoggedIn, dogsCtrl.create)
+
+router.delete('/:id', isLoggedIn, dogsCtrl.delete)
+
